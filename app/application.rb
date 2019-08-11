89f7262966d680b0e99f9021ba452ac87cc9a93a
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-11 02:42:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -10,9 +11,11 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
+      if req.path.match(/cart/)
+    @@cart.each do |item|
+      resp.write "#{item}\n"
+    end
+
     else
       resp.write "Path Not Found"
     end
