d14c13800e8d0482bbaa3e6213f33836920963af
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-11 02:45:50 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,30 +5,6 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item}\n"
-      end
-      if req.path.match(/cart/)
-    @@cart.each do |item|
-      resp.write "#{item}\n"
-      end
-
-    else
-      resp.write "Path Not Found"
-    end
-
-    resp.finish
-  end
-
-  def handle_search(search_term)
-    if @@items.include?(search_term)
-      return "#{search_term} is one of our items"
-    else
-      return "Couldn't find #{search_term}"
-    end
-  end
 end
 end
